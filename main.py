732ede6b63b61a5d7b87c6a8d4f43df0393e37bf
ANSHUMAAN KUMAR PRASAD <75872316+amino19@users.noreply.github.com>
2021-02-26 02:24:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,133 +1,193 @@
-import math
-from collections import defaultdict
-import copy
-from functools import reduce
+from typing import *
+from dataclasses import dataclass
+import random
 
 
-def chunks(lst):
-    for i in range(len(lst) - 1):
-        yield lst[i:i + 2]
+@dataclass
+class Street:
+    i_start: int
+    i_end: int
+    name: str
+    time_for_a_car_to_travel: int
+
+    cards_on_road: List['Car']
+
+    def add_car(self, car: 'Car'):
+        car.ttl = self.time_for_a_car_to_travel
+        self.cards_on_road.append(car)
+
+    def pop_waiting_car(self) -> Optional['Car']:
+        if self.cards_on_road and self.cards_on_road[0].ttl == 0:
+            return self.cards_on_road.pop(0)
+
+    def update(self):
+        for car in self.cards_on_road:
+            car.ttl = max(0, car.ttl - 1)
 
 
-class Solution:
-    def __init__(self) -> None:
-        super().__init__()
-        self.data = None
-        self.input_file_name = 'C:/Users/Anshumaan/Documents/Codes/hashcode21/c.txt'
-        self.output_file_name = 'C:/Users/Anshumaan/Documents/Codes/hashcode21/checkmate.txt'
-        self.STREETS = {}
-        self.CARS = {}
-        self.STREETS_LENS = {}
-        self.DURATION = None
-        self.NUM_INTER = None
-        self.NUM_STREETS = None
-        self.NUM_CARS = None
-        self.BONUS = None
-        self.BEST_CARS_STREET = {}
-        self.used_streets_by_cars = set()
-
-    def read_input(self, file_name):
-        with open(f'./input/{file_name}') as f:
-            self.data = [i.strip() for i in f.readlines()]
-            line_num = 0
-            self.DURATION, self.NUM_INTER, self.NUM_STREETS, self.NUM_CARS, self.BONUS = map(int, self.data[0].split(' '))
-            for _ in range(self.NUM_STREETS):
-                line_num += 1
-                line = self.data[line_num]
-                START, END, NAME, LEN = line.split(' ')
-                self.STREETS[NAME] = {'start': int(START), 'end': int(END), 'len': int(LEN)}
-                self.STREETS_LENS[NAME] = int(LEN)
-
-            for i in range(self.NUM_CARS):
-                line_num += 1
-                line = self.data[line_num]
-                CAR_STREETS = line.split(' ')[1:]
-                self.CARS[i] = list(CAR_STREETS)
-
-    def path_length(self, path):
-        path_score = 0
-        for street in path[1:]:
-            path_score += 1 + self.STREETS_LENS[street]
-        return path_score
-
-    def pop_unused_streets(self):
-        all_streets = set(street_name for street_name in self.STREETS)
-        streets_to_remove = all_streets - self.used_streets_by_cars
-        print(f'removing streets {len(streets_to_remove)}')
-        for street_name in streets_to_remove:
-            self.STREETS_LENS.pop(street_name)
-            self.STREETS.pop(street_name)
-
-    def run(self, file_name):
-        self.read_input(file_name)
-        for key, streets in self.CARS.items():
-            self.used_streets_by_cars.update(streets)
-            path_length = self.path_length(streets)
-            if path_length <= self.DURATION:
-                self.BEST_CARS_STREET[key] = path_length
-
-        self.pop_unused_streets()
-        BEST_CARS = sorted(self.BEST_CARS_STREET, key=self.BEST_CARS_STREET.get)
-
-        streets_to_intersection_map = {}
-        street_by_start = defaultdict(list)
-        street_by_end = defaultdict(list)
-        for street_slug, data in self.STREETS.items():
-            street_by_start[data['start']].append(street_slug)
-        for street_slug, data in self.STREETS.items():
-            street_by_end[data['end']].append(street_slug)
-
-        for street_slug, data in self.STREETS.items():
-            for second_street in street_by_start[data['end']]:
-                streets_to_intersection_map[f'{street_slug}__{second_street}'] = data['end']
-
-        intersection_perf = defaultdict(lambda: 0)  # street_name__int_id
-        streets_by_intersection_id = defaultdict(set)
-
-        for car_id in BEST_CARS:
-            STREETS_TO_GO = self.CARS[car_id]
-            for item in chunks(STREETS_TO_GO):
-                first_street, second_street = item
-                intersection_id = streets_to_intersection_map[f'{first_street}__{second_street}']
-                intersection_perf[f'{first_street}__{intersection_id}'] += 1
-                streets_by_intersection_id[intersection_id].add(first_street)
-
-        result_data = {}
-        intersection_perf_ratio = {}
-        for intersection_id, streets in streets_by_intersection_id.items():
-            numbers = []
-            for street in streets:
-                numbers.append(intersection_perf[f'{street}__{intersection_id}'])
-            if len(numbers) > 2:
-                gcd = reduce(lambda x, y: math.gcd(x, y), numbers)
-            elif len(numbers) == 2:
-                gcd = math.gcd(*numbers)
-            else:
-                gcd = 1
-
-            for street in streets:
-                ratio = intersection_perf[f'{street}__{intersection_id}'] / gcd
-                intersection_perf_ratio[f'{street}__{intersection_id}'] = ratio
-                result_data[intersection_id] = {street: int(ratio)}
-
-        # for CAR_ID in BEST_CARS:
-        #     STREETS_TO_GO = self.CARS[CAR_ID]
-        #     for STREET in STREETS_TO_GO:
-        #         street = self.STREETS[STREET]
-        #         result_data[street['end']] = {STREET: 1}
-
-        result_lines = []
-        result_lines.append(str(len(result_data)))
-        for intersection_id, streets in result_data.items():
-            result_lines.append(str(intersection_id))
-            result_lines.append(str(len(streets)))
-            for street_name, seconds in streets.items():
-                result_lines.append(f'{street_name} {seconds}')
-        with open(file_name, 'w') as f:
-            f.writelines('\n'.join(result_lines))
+@dataclass
+class Car:
+    street_schedule: List[str]
+    ttl: int
 
 
-s = Solution()
-# for file_name in ['a.txt', 'b.txt', 'c.txt', 'd.txt', 'e.txt', 'f.txt']:
-for file_name in ['c.txt']:
-    s.run(file_name)
+@dataclass
+class Intersection:
+    id: int
+    in_streets: Dict[str, Street]
+    out_streets: Dict[str, Street]
+
+    schedule: List[Tuple[str, int]]
+
+    active: Optional[str] = None
+    _possibilities: Set[str] = None
+
+    def init(self):
+        self._possibilities = set(self.in_streets.keys())
+
+    def acquire(self, in_street, t):
+        if in_street in self._possibilities:
+            self.active = in_street
+            self._possibilities.remove(in_street)
+            self.schedule.append((in_street, t))
+
+    def update(self, t):
+        # STRATEGY
+
+        if self._possibilities:
+            # Random
+            # self.acquire(random.choice(list(self._possibilities)), t)
+            street_with_max_cars = None
+            max_cars = 0
+            for street_name in self._possibilities:
+                street = self.in_streets[street_name]
+                cars_on_the_street = len(street.cards_on_road)
+                if cars_on_the_street > max_cars:
+                    max_cars = cars_on_the_street
+                    street_with_max_cars = street_name
+
+            if street_with_max_cars:
+                self.acquire(street_with_max_cars, t=t)
+
+        # END STRATEGY
+
+        for street in self.in_streets.values():
+            street.update()
+
+        if self.active:
+            in_street = self.in_streets[self.active]
+            if in_street.cards_on_road:
+                car = in_street.pop_waiting_car()
+                if car:
+                    if car.street_schedule:
+                        out_street = car.street_schedule.pop(0)
+                        self.out_streets[out_street].add_car(car)
+
+    def to_submission(self, duration) -> List[Tuple[str, int]]:
+        if not self.schedule:
+            # Fill not activated intersections
+            for t, s in enumerate(self.in_streets.keys()):
+                self.schedule.append((s, t))
+
+        start_times = []
+        for street, started_at in self.schedule:
+            start_times.append(started_at)
+
+        start_times.append(duration)
+
+        fmt = []
+        for (street, started_at), ended_at in zip(self.schedule, start_times[1:]):
+            fmt.append((street, ended_at - started_at))
+
+        return fmt
+
+
+def solve(duration: int,
+          n_cars: int,
+          bonus_points: int,
+          streets: Dict[str, Street],
+          intersections: Dict[int, Intersection]):
+
+    for intersection in intersections.values():
+        intersection.init()
+
+    print('Starting simulations')
+    for t in range(duration):
+        if t % 1000 == 0:
+            print(f't={t}')
+        for intersection in intersections.values():
+            intersection.update(t)
+
+
+def main(filename: str):
+    print(f'Solving {filename}')
+    with open(filename, encoding='utf-8') as f:
+        duration, n_intersections, n_streets, n_cars, bonus_points = \
+            list(map(int, f.readline().strip().split()))
+
+        streets = {}
+        intersections: Dict[int, Intersection] = {}
+
+        for i in range(n_streets):
+            i_start, i_end, name, time = f.readline().split()
+            street = Street(
+                int(i_start),
+                int(i_end),
+                name,
+                int(time),
+                cards_on_road=[]
+            )
+
+            streets[name] = street
+
+            if street.i_start not in intersections:
+                intersections[street.i_start] = Intersection(
+                    id=street.i_start,
+                    in_streets={},
+                    out_streets={},
+                    active=None,
+                    schedule=[]
+                )
+
+            if street.i_end not in intersections:
+                intersections[street.i_end] = Intersection(
+                    id=street.i_end,
+                    in_streets={},
+                    out_streets={},
+                    active=None,
+                    schedule=[]
+                )
+
+            intersections[street.i_start].out_streets[street.name] = street
+            intersections[street.i_end].in_streets[street.name] = street
+
+        for i in range(n_cars):
+            car = Car(f.readline().split()[1:], 0)
+
+            # Place car at the end of the street
+            streets[car.street_schedule.pop(0)].add_car(car)
+
+        solve(duration, n_cars, bonus_points, streets, intersections)
+
+    with open(f'{filename}.out.txt', 'w', encoding='utf-8') as f:
+        f.write(str(len(intersections)))
+        f.write('\n')
+        for intersection in intersections.values():
+            f.write(str(intersection.id))
+            f.write('\n')
+
+            intersection_submission = intersection.to_submission(duration=duration)
+            f.write(str(len(intersection_submission)))
+            f.write('\n')
+            for street_name, time in intersection_submission:
+                f.write(f'{street_name} {time}')
+                f.write('\n')
+
+
+if __name__ == '__main__':
+    # main('a.txt')
+    # main('b.txt')
+    # main('c.txt')
+    # main('d.txt')
+    # main('e.txt')
+    main('f.txt')
